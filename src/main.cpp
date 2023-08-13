12388ff34b021e04026aaa362f7a406a8bf9d7c1
rafafelps <rafaelfelipe@alunos.utfpr.edu.br>
2023-08-13 03:20:21 -0300

    Generalized for various polygons

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,39 @@
 #include <cmath>
 
 #define RADIUS 350
-#define DELAY 0
-#define SIDES 6
+#define DELAY 0.0
+#define SIDES 5
 #define MAX_POINTS 40000
+#define RULE (1.0 / 1.618033988749)
 
 const float ratio = 16.0 / 9.0;
 const int height = 720;
 const int width = height * ratio;
 
-typedef struct {
-    Vector2 pos;
-    Color color;
-} Point;
-
-void update(Vector2* vertexList, double* lastTime, Point** pointsList, unsigned int* pointsCounter);
-void render(Point** pointsList, unsigned int pointsCounter, RenderTexture2D* renderTexture);
+void update(Vector2* vertexList, double* lastTime, Vector2* pointsList, unsigned int* pointsCounter);
+void render(Vector2* pointsList, unsigned int pointsCounter, RenderTexture2D* renderTexture);
 
 int main() {
     InitWindow(width, height, "Chaos Game");
     SetTargetFPS(60);
 
     Vector2 center = {width * 0.5, height * 0.5};
-    float cos30 = cosf(30 * (PI / 180.0));
-    Vector2 vertexList[SIDES] = {{center.x - RADIUS, center.y},
-                         {center.x - (RADIUS / 2), center.y - (RADIUS * cos30)},
-                         {center.x + (RADIUS / 2), center.y - (RADIUS * cos30)},
-                         {center.x + RADIUS, center.y},
-                         {center.x + (RADIUS / 2), center.y + (RADIUS * cos30)},
-                         {center.x - (RADIUS / 2), center.y + (RADIUS * cos30)}};
+    Vector2 vertexList[SIDES];
+    for (int i = 0; i < SIDES; i++) {
+        float theta = PI / 2.0 - i * (2 * PI / SIDES);
+        vertexList[i].x = center.x + RADIUS * cosf(theta);
+        vertexList[i].y = center.y - RADIUS * sinf(theta);
+    }
 
-    Point* pointsList[MAX_POINTS];
+    Vector2 pointsList[MAX_POINTS];
     unsigned int pointsCounter = 0;
-    pointsList[0] = (Point*) malloc(sizeof(Point));
-    pointsList[0]->pos = vertexList[0];
-    pointsList[0]->color = BLUE;
+    pointsList[0] = vertexList[0];
     pointsCounter++;
 
     RenderTexture2D renderTexture = LoadRenderTexture(width, height);
     BeginTextureMode(renderTexture);
     // Hexagon
-    DrawPolyLines(center, SIDES, RADIUS, 90, RAYWHITE);
+    DrawPolyLines(center, SIDES, RADIUS, 180, RAYWHITE);
     EndTextureMode();
 
     double lastTime = GetTime();
@@ -58,20 +51,18 @@ int main() {
     return 0;
 }
 
-void update(Vector2* vertexList, double* lastTime, Point** pointsList, unsigned int* pointsCounter) {
+void update(Vector2* vertexList, double* lastTime, Vector2* pointsList, unsigned int* pointsCounter) {
     double currentTime = GetTime();
     if (currentTime - *lastTime >= DELAY) {
         if (*pointsCounter >= MAX_POINTS) { *lastTime = currentTime; return; }
         
-        unsigned char currVertex = rand() % 6;
+        unsigned char currVertex = rand() % SIDES;
 
         Vector2 vec;
-        vec.x = (vertexList[currVertex].x - pointsList[*pointsCounter - 1]->pos.x) * (2.0 / 3.0) + pointsList[*pointsCounter - 1]->pos.x;
-        vec.y = (vertexList[currVertex].y - pointsList[*pointsCounter - 1]->pos.y) * (2.0 / 3.0) + pointsList[*pointsCounter - 1]->pos.y;
+        vec.x = (vertexList[currVertex].x - pointsList[*pointsCounter - 1].x) * RULE + pointsList[*pointsCounter - 1].x;
+        vec.y = (vertexList[currVertex].y - pointsList[*pointsCounter - 1].y) * RULE + pointsList[*pointsCounter - 1].y;
 
-        pointsList[*pointsCounter] = (Point*) malloc(sizeof(Point));
-        pointsList[*pointsCounter]->pos = vec;
-        pointsList[*pointsCounter]->color = BLUE;
+        pointsList[*pointsCounter] = vec;
 
         *pointsCounter += 1;
 
@@ -79,17 +70,18 @@ void update(Vector2* vertexList, double* lastTime, Point** pointsList, unsigned 
     }
 }
 
-void render(Point** pointsList, unsigned int pointsCounter, RenderTexture2D* renderTexture) {
+void render(Vector2* pointsList, unsigned int pointsCounter, RenderTexture2D* renderTexture) {
     if (pointsCounter < MAX_POINTS) {
         BeginTextureMode(*renderTexture);
-        DrawCircleV(pointsList[pointsCounter - 1]->pos, 1, pointsList[pointsCounter - 1]->color);
+        DrawCircleV(pointsList[pointsCounter - 1], 1, BLUE);
         EndTextureMode();
     }
 
     BeginDrawing();
     ClearBackground(BLACK);
 
-    DrawTexture(renderTexture->texture, 0, 0, WHITE);
+    // NOTE: Render texture must be y-flipped due to default OpenGL coordinates (left-bottom)
+    DrawTextureRec(renderTexture->texture, (Rectangle) { 0, 0, (float)renderTexture->texture.width, (float)-renderTexture->texture.height }, (Vector2) { 0, 0 }, WHITE);
 
     // Counter
     std::string s = std::to_string(pointsCounter);
